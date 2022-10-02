ee1b41e0e0629d8387b0b30b90085c3bdb7f9a43
Michel Morais Ferreira <michelmf@poli.ufrj.br>
2022-10-02 03:06:10 -0300

    Create diagram for the project

---
+++ diagrams/diagram.py
@@ -0,0 +1,28 @@
+"""
+Diagrams.py
+Generate the infrastructure's diagram of the CMS article project.
+"""
+from diagrams import Cluster, Diagram
+
+from diagrams.azure.compute import AppServices
+from diagrams.azure.database import SQLDatabases
+from diagrams.azure.storage import StorageAccounts
+
+with Diagram("CMS Article Project", show=False) as diagram:
+    
+    with Cluster("          Resource Group Name: rg_article_cms          "):
+      
+        with Cluster("     Compute Services     "):
+            compute = AppServices("cms_app_service")
+
+        with Cluster("     Storage Services     ") as databases_cluster:
+            databases = [
+                SQLDatabases("cms_sql_database"),
+                StorageAccounts("cms_storage_account")
+            ]
+
+            compute >> databases
+            compute << databases
+
+# Create the diagram
+diagram
